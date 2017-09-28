f134232e900fb85d5d1d7e5f7af409b0cb118491
mixelpixel <mixelpix@mac.com>
2017-09-28 04:14:08 -0400

    idem

--- HDDseek.js
+++ HDDseek.js
@@ -1,46 +1,51 @@
-/**********************************************************
-invoke: `node <script.js> opt: milliseconds MegaBytes KB`
-example `node HDDseek.js 9 100 4`
-**********************************************************/
-/* eslint no-console: 0 */
 'use-strict';
+/* eslint no-console: 0 */
 const process = require('process'); // <~~~ Not needed, just making linter happy
 
-// HDD seek time in milliseconds (defauts to 9)
-const seekTime = process.argv[2] || 9;
+/**********************************************************
+invoke: `node <script.js> opt: dataSizeMB seekTime_ms blockSizeKB`
+example `node HDDseek.js 100 9 4`
+**********************************************************/
 
 // file size entered in Megabytes (defaults to 100MB)
-const fileSizeMB = process.argv[3] || 100;
-// const fileSizeMB = process.argv[3] || 1;
+const dataSizeMB = Number(process.argv[2]) || 100;
 // convert MB to KB
-const fileSizeKB = fileSizeMB * 1024;
+const dataSizeKB = dataSizeMB * 1024;
+
+// HDD seek time in milliseconds (defauts to 9)
+const seekTime = Number(process.argv[3]) || 9;
 
 // HDD block size in KB (default 4KB)
-const blockSize = process.argv[4] || 4;
+const blockSize = Number(process.argv[4]) || 4;
+// console.log(process.argv);
 
 // Target number of blocks to seek which amount to file size
-const target = fileSizeKB / blockSize;
+const target = dataSizeKB / blockSize;
 // total data sought in KB
 let data = 0;
 console.log(`Total data sought: ${data}`);
 // total number of data blocks sought
 let blocks = 0;
 console.log(`Total # of blocks: ${blocks}`);
+console.log(`${seekTime}, ${dataSizeMB}, ${dataSizeKB}, ${blockSize}, ${target}, ${data}, ${blocks}`);
 
-console.log(`${seekTime}, ${fileSizeMB}, ${fileSizeKB}, ${blockSize}, ${target}, ${data}, ${blocks}`);
+// program timer
 const timeStart = process.hrtime();
-
 // HDD seek emulator
 const seek = () => {
-  if(data === fileSizeKB) {
-    clearInterval(seek);
-    console.log(`In theory it takes ${blocks * seekTime / 1000} seconds to seek ${fileSizeMB}MB of data ${blockSize}KB at a time.`);
+  if(data === dataSizeKB) {
+    clearInterval(cycle);
+    console.log(`   "In theory it only takes ${blocks * seekTime / 1000} seconds to seek ${dataSizeMB}MB`);
+    console.log(`    of HDD data while seeking out ${blockSize}KB blocks at a time.\n`);
     const timeEnd = process.hrtime(timeStart);
-    console.log(`In practice, it took roughly ${timeEnd[0]} seconds and ${timeEnd[1]} nanoseconds`);
+    console.log(`    In practice, it took roughly ${timeEnd[0] + (timeEnd[1] / 1000000000)} seconds`);
+    console.log(`    Yeah, yeah, about ${timeEnd[0]}, ...${timeEnd[0] + (timeEnd[1] / 1000000000)} seconds. Yeah. ${timeEnd[0]}.`);
+    console.log('    ...\n    BAD BABY - HOT WATER!!!!!"');
+    console.log('    -- "Raymond Babbit" as played by Dustin Hoffman (1998, "Rain Man")\n');
     return;
   }
   data += blockSize;
-  console.log(`Target for data sought: ${fileSizeKB}KB`);
+  console.log(`Target for data sought: ${dataSizeKB}KB`);
   console.log(`Total data sought:      ${data}KB\n`);
   blocks++;
   console.log(`Target # of blocks:     ${target}`);
